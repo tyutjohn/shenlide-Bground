c6e5226640a92bcf7ee6a0ad61a51361be42b328
tyutjohn <867273526@qq.com>
2019-11-17 11:54:15 +0800

    fix bug

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@
  * @Github: https://github.com/tyutjohn
  */
 import Vue from 'vue';
-import Vuex from 'Vuex';
+import Vuex from 'vuex';
 import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
